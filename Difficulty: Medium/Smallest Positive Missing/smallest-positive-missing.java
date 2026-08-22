725108a80a70c246cecea0c556468008d457b247
pritamsinghrathore78988 <pritamsinghrathore78988@gmail.com>
2026-08-23 00:22:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int missingNumber(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            while (arr[i] >= 1 && arr[i] <= n
+                    && arr[arr[i] - 1] != arr[i]) {
+                int correctIndex = arr[i] - 1;
+                int temp = arr[i];
+                arr[i] = arr[correctIndex];
+                arr[correctIndex] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+}
